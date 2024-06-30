7fbe9b9dfacf325b827ac335a5cdf666ba742e53
Soham Pagi <paguisoham36@gmail.com>
2024-06-30 21:52:21 +0530

    Initial commit

---
+++ src/UI/Header.svelte
@@ -0,0 +1,24 @@
+<header>
+  <h1>MeetUs</h1>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4rem;
+    background: #cf0056;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 2.6);
+  }
+
+  h1 {
+    color: white;
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    margin: 0;
+  }
+</style>
